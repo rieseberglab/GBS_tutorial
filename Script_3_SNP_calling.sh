a149f45d69fd53d952668ba25c8ff68dc75d187b
Yue Yu <46236627+yueyu27@users.noreply.github.com>
2026-03-31 10:10:00 -0700

    Updated SNP calling and filtering code

--- Script_3_SNP_calling.sh
+++ Script_3_SNP_calling.sh
@@ -107,7 +107,7 @@ SAMPLE=$(echo $i | cut -d "." -f 1)
 gatk --java-options "-Xmx28G" HaplotypeCaller \
 	 -R ${REF} \
 	 -I ${SAMPLE}.callonPRAHap2.sort.bam \
-	 -O GVCF_output/${SAMPLE}.callonPRAHap2.g.vcf \
+	 -O /home/yueyu/scratch/GBS/GVCF_output/${SAMPLE}.callonPRAHap2.g.vcf \
 	 -ERC GVCF \
 	 --max-alternate-alleles 3 \
 	 --pcr-indel-model AGGRESSIVE \
@@ -122,7 +122,75 @@ gatk --java-options "-Xmx28G" HaplotypeCaller \
 #####################################
 
 
+#---  Move all GVCF to the same folder
+cd /home/yueyu/scratch/GBS/GVCF_output
 
+#---  Create sample map (required to create the DBI database)
+cd /home/yueyu/scratch/GBS/GVCF_output
+
+for i in *.g.vcf; do
+    name=$(echo "$i" | cut -d "." -f 1)
+    echo -e "$name\t$i" >> sample_names.txt
+done
+
+head sample_names.txt
+
+
+# -- Import all samples into DBI per CHROMOSOME 
+
+#                 |------> Chr01
+#  G.VCF (Genome)-|------> Chr02
+#                 |------> Chr03
+#                 |------> ChrXX
+
+
+cd /home/yueyu/scratch/GBS/GVCF_output
+
+for i in $(seq -w 01 17)
+do
+  cat <<EOL > PRA_Chr${i}_makeDB.sh
+#!/bin/bash
+#SBATCH --account=rrg-rieseber-ac
+#SBATCH --time=10:00:00
+#SBATCH --cpus-per-task=3
+#SBATCH --mem-per-cpu=10G
+
+module load StdEnv/2020
+module load gatk/4.2.4.0
+
+cd /home/yueyu/scratch/GBS/GVCF_output
+
+gatk --java-options "-Xmx48g" GenomicsDBImport \\
+    --genomicsdb-workspace-path PRA_CHROM$i \\
+    --batch-size 50 \\
+    --sample-name-map sample_names.txt \\
+    --reader-threads 3 \\
+    -L PRA_chr$i
+EOL
+
+  chmod +x PRA_Chr${i}_makeDB.sh
+done
+
+
+# -- Make final run script
+for i in {01..17};
+do 
+    echo "sbatch PRA_Chr${i}_makeDB.sh" >> run_DBI_byCHROM.sh
+done
+
+
+#-- Run in parallel 
+cat run_DBI_byCHROM.sh | parallel -j 17
+
+
+#-- Check status
+for i in {30639408..30639424};do
+    seff $i | grep State >> check_PRA_makeDB.txt
+done
+
+#-- Remove files
+rm slurm-*
+rm check_PRA_makeDB.txt
 
 
 
@@ -132,6 +200,97 @@ gatk --java-options "-Xmx28G" HaplotypeCaller \
 #  3.4 GenotypeGVCF:  database -> VCF
 #####################################
 
+cd /home/yueyu/scratch/ALL_GBS_call_on_PRA/VCF
+
+for i in $(seq -w 01 17)
+do
+  cat <<EOL > PRA_Chr${i}_genotypeVCF.sh
+#!/bin/bash
+#SBATCH --account=rrg-rieseber-ac
+#SBATCH --time=10:00:00
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=5G
+
+module load StdEnv/2020
+module load gatk/4.2.4.0
+
+cd /home/yueyu/scratch/ALL_GBS_call_on_PRA/G_VCF_ALL_merged
+
+gatk --java-options "-Xmx90g" GenotypeGVCFs \\
+     -R /home/yueyu/scratch/PRA_FASTA/praecox2.fasta \\
+     -V gendb://PRA_CHROM$i \\
+     -O /home/yueyu/scratch/ALL_GBS_call_on_PRA/VCF/PRA_Chr$i.vcf.gz
+EOL
+
+  chmod +x PRA_Chr${i}_genotypeVCF.sh
+done
+
+
+#Make final run script
+for i in {01..17};
+do 
+    echo "sbatch PRA_Chr${i}_genotypeVCF.sh" >> run_PRA_GT_byCHR.sh
+done
+
+
+#Run in parallel
+cat run_PRA_GT_byCHR.sh | parallel -j 17
+
+
+
+
+
+#--- Merge all CHROM into final VCF
+
+#                 |------> Chr01
+#  G.VCF (Genome)-|------> Chr02 ---> Merge, Final VCF
+#                 |------> Chr03
+#                 |------> ChrXX
+
+
+nano run_PRA_vcf_merge.sh
+
+#----------  vcf_merge.sh  ---------- 
+#!/bin/bash
+#SBATCH --account=rrg-rieseber-ac
+#SBATCH --time=1:00:00
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=10G
+
+module load StdEnv/2020
+module load gcc/9.3.0
+module load bcftools/1.16
+module load tabix/0.2.6
+
+
+cd /home/yueyu/scratch/ALL_GBS_call_on_PRA/VCF
+
+bcftools concat \
+  PRA_Chr01.vcf.gz \
+  PRA_Chr02.vcf.gz \
+  PRA_Chr03.vcf.gz \
+  PRA_Chr04.vcf.gz \
+  PRA_Chr05.vcf.gz \
+  PRA_Chr06.vcf.gz \
+  PRA_Chr07.vcf.gz \
+  PRA_Chr08.vcf.gz \
+  PRA_Chr09.vcf.gz \
+  PRA_Chr10.vcf.gz \
+  PRA_Chr11.vcf.gz \
+  PRA_Chr12.vcf.gz \
+  PRA_Chr13.vcf.gz \
+  PRA_Chr14.vcf.gz \
+  PRA_Chr15.vcf.gz \
+  PRA_Chr16.vcf.gz \
+  PRA_Chr17.vcf.gz \
+  -O z > cd /home/yueyu/scratch/ALL_GBS_call_on_PRA/VCF/Raw_VCF.vcf.gz
+
+tabix -p vcf Raw_VCF.vcf.gz
+
+#----------  vcf_merge.sh  (END) ---------- 
+
+# -- Check number of SNPs in the raw VCF file
+bcftools view -H Raw_VCF.vcf.gz | wc -l 
 
 
 # END
--- Script_4_SNP_filtering.sh
+++ Script_4_SNP_filtering.sh
@@ -8,171 +8,308 @@
 #
 #####################################
 
-
-
 # This code includes the following:
-# 4.1: SNP filtering (VCFtools)
-#      quality, minor allele counts, % found accross individuals
-#      filter out individuals with lots of missing data
-#      A script to count the number of potential genotyping errors due to low read depth
+
+# -- OVERVIEW GBS SNP FILTERING STEPS
+
+# -- Step 1: Check your data ( no code )
+# -- Step 2: Extract SNPs/INDELs
+# -- Step 3: Plot out parameters for INFO field 
+# -- Step 4: GATK hard filter on INFO field for reliable SNPs (NOT related to N sample size) AND general loose ExcessHet filter
+# -- Step 5: Check per sample DP and GQ before sample-level filters
+# -- Step 6: GATK filter on SAMPLE field for SAMPLE-DP and SAMPLE-GQ (NOT related to N sample size)
+# -- Step 7: Remove sample with low coverage
+# -- Step 8: Recode INFO field
+# -- Step 9: GATK filter on SAMPLE field pop specific parameters (YES related to N sample size), eg. MAF/MAC/missing rate etc...
+
+
+
+#--------------------------
+#  Step 2: Extract SNPs
+#--------------------------
+
+nano Filter_Step1.sh
+# ----------- Filter_Step1.sh ---------
+#!/bin/bash
+#SBATCH --account=rrg-rieseber-ac 
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=10G
+#SBATCH --time=2:00:00
+
+cd /home/yueyu/scratch/GBS/VCF_DEB_FILTERED
+
+module load StdEnv/2023
+module load gatk/4.6.1.0
+
+unset JAVA_TOOL_OPTIONS
+
+#SNP
+gatk --java-options "-Xmx8g" SelectVariants \
+  -V /home/yueyu/scratch/GBS/VCF_DEB/Raw_VCF.vcf.gz \
+  --select-type-to-include SNP \
+  --exclude-filtered \
+  -O SNP_FILTERED.vcf.gz
+# ----------- Filter_Step1.sh (END)---------
 
 
 
 
-#####################################
-#  4.1 SNP filtering (VCFtools)
-#####################################
-
-# -- raw VCF file with 40 samples (10 individuals/populations)
-# Note: Four populations in this VCF are: GEN, BARD, KL, PC
-
-cd /scratch/celphin/GBS_workshop/0_Raw_data
+#--------------------------
+#  Step 3: Plot SNP quality (DO NOT RUN)
+#--------------------------
+module load StdEnv/2023  
+module load gcc/12.3
+module load bcftools/1.19
 
 
-# -- load vcftools/bcftools
-module load StdEnv/2020 
-module load vcftools/0.1.16
-module load gnuplot/5.4.2
+#Extract info and plot in R 
+bcftools query SNP_v20250415.vcf.gz -f '%ExcessHet\t%FS\t%SOR\t%MQRankSum\t%ReadPosRankSum\t%QD\t%MQ\t%DP\n' > SNP_v20250415_HET.FS.SOR.MQRS.RPRS.QD.MQ.DP.txt
+wc -l SNP_v20250415_HET.FS.SOR.MQRS.RPRS.QD.MQ.DP.txt
+# 2,342,801 (2 million SNPs - DEB)
+
+
+module load StdEnv/2023
+module load r/4.4.0
+
+R
+# ------------- in R -------------
+library(data.table)
+
+# Load DEB SNP table
+snps <- fread("SNP_v20250415_HET.FS.SOR.MQRS.RPRS.QD.MQ.DP.txt", na.strings = c(".", "NA"), colClasses = "numeric", header = F)
+
+# Load PRA SNP table
+# snps <- fread("PRA_SNP_v20250415_HET.FS.SOR.MQRS.RPRS.QD.MQ.DP.txt", na.strings = c(".", "NA"), colClasses = "numeric", header = F)
+
+str(snps)
+colnames(snps) <- c("ExcessHet","FS","SOR","MQRankSum","ReadPosRankSum","QD","MQ","DP")
+
+# !!! IMPORTANT!! change saved PNG names
+
+# -- ExcessHet
+png(filename = "ExcessHet_v20150415.png")
+dExcessHet <- density(snps$ExcessHet,na.rm=T)
+plot(dExcessHet,main="ExcessHet distribution", xlab="ExcessHet")
+dev.off()
+
+# -- FS
+# or can plot FS with a log base 10 scale
+png(filename = "FS_v20150415.png")
+dFS <- density(snps$FS,na.rm=T)
+plot(dFS,main="FS distribution", xlab="FS")
+dev.off()
+
+# -- SOR
+png(filename = "SOR_v20150415.png")
+dSOR <- density(snps$SOR,na.rm=T)
+plot(dSOR,main="SOR distribution", xlab="SOR")
+dev.off()
+
+# -- MQRankSum
+png(filename = "MQRankSum_v20150415.png")
+dMQRankSum <- density(snps$MQRankSum,na.rm=T)
+plot(dMQRankSum,main="MQRankSum distribution", xlab="MQRankSum")
+dev.off()
+
+# -- ReadPosRankSum
+png(filename = "ReadPosRankSum_v20150415.png")
+dReadPosRankSum <- density(snps$ReadPosRankSum,na.rm=T)
+plot(dReadPosRankSum,main="ReadPosRankSum distribution", xlab="ReadPosRankSum")
+dev.off()
+
+# -- QD
+png(filename = "QD_v20150415.png")
+dQD <- density(snps$QD,na.rm=T)
+plot(dQD,main="QD distribution", xlab="QD")
+dev.off()
+
+# -- MQ
+png(filename = "MQ_v20150415.png")
+dMQ <- density(snps$MQ,na.rm=T)
+plot(dMQ,main="MQ distribution", xlab="MQ")
+dev.off()
+
+# -- DP
+png(filename = "DP_v20150415.png")
+dDP <- density(snps$DP,na.rm=T)
+plot(dDP,main="DP distribution", xlab="DP")
+dev.off()
+
+
+
+#--------------------------
+#  Step 4: Hard filter
+#--------------------------
+
+nano Filter_Step2.sh
+# ----------- Filter_Step2.sh ---------
+#!/bin/bash
+#SBATCH --account=rrg-rieseber-ac
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=10G
+#SBATCH --time=1:00:00
+
+cd /home/yueyu/scratch/GBS/VCF_DEB_FILTERED
+
+module load StdEnv/2023
+module load gatk/4.6.1.0
+
+unset JAVA_TOOL_OPTIONS
+
+gatk --java-options "-Xmx8g" VariantFiltration \
+    -V SNP_FILTERED.vcf.gz \
+    -filter "QD < 2.0" --filter-name "QD2" \
+    -filter "FS > 60.0" --filter-name "FS60" \
+    -filter "MQ < 40.0" --filter-name "MQ40" \
+    -filter "MQRankSum < -12.5" --filter-name "MQRankSumNeg12.5" \
+    -filter "ExcessHet > 54.69" --filter-name "ExcessHet5469" \
+    -O SNP_INFO_MARKED.vcf.gz
+
+
+# ADD CODE TO EXCLUDE THE FILTERED
+gatk --java-options "-Xmx8g" SelectVariants \
+  -V SNP_INFO_MARKED.vcf.gz \
+  --exclude-filtered \
+  -O SNP_INFO_FILTERED.vcf.gz
+
+# ----------- Filter_Step2.sh (END)---------
+
+
+# --------------------
+# Step 5: Check per sample DP and GQ before sample-level filters
+# --------------------
+
+# Example
+# ===========
+#  (Genotype) GQ, mean ~ 58 
+# ===========
+# ===========
+#  (Genotype) DP, mean ~ 22
+# ===========
+
+
+# --------------------
+# Step 6: BCFTOOLS on SAMPLE field for SAMPLE-DP and SAMPLE-GQ (Zhe code, very good!)
+# --------------------
+
+
+
+nano Filter_Step3.sh
+# ----------- Filter_Step3.sh ---------
+#!/bin/bash
+#SBATCH --account=rrg-rieseber-ac
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=20G
+#SBATCH --time=2:00:00
+
+cd /home/yueyu/scratch/GBS/VCF_DEB_FILTERED
+
+module load StdEnv/2020
 module load gcc/9.3.0
 module load bcftools/1.16
-module load plink/1.9b_6.21-x86_64
+module load tabix/0.2.6
+
+bcftools filter \
+    -e '(GQ<30 | FORMAT/DP<10)' \
+    SNP_INFO_FILTERED.vcf.gz \
+    -S . \
+    -s "PASS" \
+    -Oz -o INFO_GENO_FILTERED_single_sep.vcf.gz
+
+tabix -p vcf INFO_GENO_FILTERED_single_sep.vcf.gz
+
+# ----------- Filter_Step3.sh (END)---------
 
 
-# -- Count number of sites (SNPs + others)
-grep -v '^#' TotalRawSNPs_subset_40samples.vcf | wc -l
-# 559,815 Variants
+# --------------------
+# Step 7: FILTER MAX MISSINGNESS (site level)
+# --------------------
+nano Filter_Step4.sh
+# ----------- Filter_Step4.sh ---------
+#!/bin/bash
+#SBATCH --account=rrg-rieseber-ac
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=20G
+#SBATCH --time=1:00:00
+
+cd /home/yueyu/scratch/GBS/VCF_DEB_FILTERED
+
+module load StdEnv/2023
+module load gatk/4.6.1.0
+
+unset JAVA_TOOL_OPTIONS
+
+gatk --java-options "-Xmx18g" SelectVariants \
+            -V SNP_INFO_GENO_FILTERED_single_sep.vcf.gz \
+            --remove-unused-alternates \
+            --restrict-alleles-to BIALLELIC \
+            --max-nocall-fraction 0.3 \
+            -O SNP_INFO_GENO_BI_NOCALL03_FILTERED.vcf.gz
+
+# ----------- Filter_Step4.sh (END)---------
 
 
+# --------------------
+# Step 8: REMOVE SAMPLES THAT ARE OF LOW COVERAGE -- YES, do it after filter for missing rate (of the sites)
+# -------------------- 
 
-# ---------------------------------------------------
-#  filtering SNP step 1 : start new computing session 
-# ---------------------------------------------------
-tmux new-session -s test
-tmux attach-session -t test
+module load StdEnv/2020 
+module load vcftools/0.1.16
 
-salloc -c1 --time 03:00:00 --mem 30G --account def-rieseber #(change this to your account)
-
-
-
-cd /scratch/celphin/GBS_workshop/3_SNP_filtering
-
-# Copy the raw VCF file to the current directory
-cp /scratch/celphin/GBS_workshop/0_Raw_data/TotalRawSNPs_subset_40samples.vcf .
-
-ls
-
-
-
-# ---------------------------------------------------
-#  filtering SNP step 2 : HWE filter (high heterzygosity)
-# ---------------------------------------------------
-# convert the vcf file to Plink
-vcftools --vcf TotalRawSNPs_subset_40samples.vcf --plink --out TotalRawSNPs_plink
-
-# find SNPs heterozygous in all or 90% of individuals 
-plink --file TotalRawSNPs_plink --hardy --out TotalRawSNPs_hardy
-
-more TotalRawSNPs_hardy.hwe
-
-# find SNPs with high heterozygosity
-mawk '$7 > 0.4' TotalRawSNPs_hardy.hwe | cut -f1 > HighHetSNPs_40.txt 
-mawk '$7 > 0.5' TotalRawSNPs_hardy.hwe | cut -f1 > HighHetSNPs_50.txt 
-mawk '$7 > 0.6' TotalRawSNPs_hardy.hwe | cut -f1 > HighHetSNPs_60.txt 
-mawk '$7 > 0.7' TotalRawSNPs_hardy.hwe | cut -f1 > HighHetSNPs_70.txt 
-mawk '$7 > 0.8' TotalRawSNPs_hardy.hwe | cut -f1 > HighHetSNPs_80.txt
-mawk '$7 > 0.9' TotalRawSNPs_hardy.hwe | cut -f1 > HighHetSNPs_90.txt
-
-wc -l HighHetSNPs_40.txt
-#8017 HighHetSNPs_40.txt
-wc -l HighHetSNPs_50.txt
-#5074 HighHetSNPs_50.txt
-wc -l HighHetSNPs_60.txt
-#3813 HighHetSNPs_60.txt
-wc -l HighHetSNPs_70.txt
-#2969 HighHetSNPs_70.txt
-wc -l HighHetSNPs_80.txt
-#2342 HighHetSNPs_80.txt
-wc -l HighHetSNPs_90.txt
-#2000 HighHetSNPs_90.txt
-
-
-# get list of SNP IDs
-awk '{print $2}' HighHetSNPs_60.txt > HighHetSNPs_60_list
-head HighHetSNPs_60_list
-
-# Change names into 2 columns
-sed 's/:/\t/g' HighHetSNPs_60_list > HighHetSNPs_60_list.txt
-head HighHetSNPs_60_list.txt
-
-
-# filter out this list of SNPs that are 60% het across all populations
-VCF="TotalRawSNPs_subset_40samples.vcf"
-LIST="HighHetSNPs_60_list.txt"
-
-vcftools --vcf $VCF --exclude-positions $LIST --recode --recode-INFO-all --out TotalRawSNPs_rmhet
-# After filtering, kept 556,003 out of a possible 559,815 Sites
+cd /home/yueyu/scratch/GBS/VCF_DEB_FILTERED
+vcftools --gzvcf SNP_INFO_GENO_BI_NOCALL03_FILTERED.vcf.gz --missing-indv --out sample_missing_ratio
+awk '$5 > 0.3' sample_missing_ratio.imiss
 
 
 
-
-# ---------------------------------------------------
-#  filtering SNP step 3 : Rename samples in vcf file
-# ---------------------------------------------------
-# -- https://www.biostars.org/p/279195/ 
-
-bcftools query -l TotalRawSNPs_rmhet.recode.vcf > sample_names.txt
-
-#sed 's/,/ /g' M_caridnalis_samples_renamed.csv > M_caridnalis_samples_renamed.txt
-#bcftools reheader -s M_caridnalis_samples_renamed.txt -o Mimulus_timeseries_filtered_variants_rename.vcf Mimulus_timeseries_filtered_variants.vcf
-#bcftools query -l Mimulus_timeseries_filtered_variants_rename.vcf
+# ------------------------------------
+# Step 9: RECODE INFO + AF filter
+# -----------------------------------
 
 
+nano Filter_Step5.sh
+# ----------- Filter_Step5.sh ---------
+#!/bin/bash
+#SBATCH --account=rrg-rieseber-ac
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=5G
+#SBATCH --time=2:00:00
 
-# ---------------------------------------------------
-#  filtering SNP step 4 : Site-level Filtering
-# ---------------------------------------------------
-# -- ON the following:
+cd /home/yueyu/scratch/GBS/VCF_DEB_FILTERED
 
-# -> high quality
-# -> remove indels
-# -> biallelic
-# -> missing data < 90%
-# -> monomorphic
-# -> LD
-# -> minor allele freq of 0.05 (0.05 * 40 * 2 = 4 alleles)
+module load StdEnv/2020
+module load gcc/9.3.0
+module load bcftools/1.16
+module load tabix/0.2.6
 
-# -- Code
-vcftools --vcf TotalRawSNPs_rmhet.recode.vcf \
---minGQ 30 \
---remove-indels \
---max-alleles 2 \
---max-missing 0.9 \
---min-alleles 2 \
---thin 300 \ # filteirng for LD (1 SNP to every 300bp)
---maf 0.05 \
---recode \
---recode-INFO-all \
---out Cassiope_noMERhb
+# not needed and recode information (INFO - AN, AC, AF, DP need to recode for downstream)
+# Remove other INFO field that are not accurate anymore and can not be re-calculated after filtering (eg. require initial seq info)
+# ^ means to keep these INFO fields but drop the rest
 
-# After filtering, kept 10,857 out of a possible 556,003 Sites
+bcftools annotate -x "^INFO/AN,INFO/AC,INFO/AF,INFO/DP" SNP_INFO_GENO_BI_NOCALL03_FILTERED.vcf.gz -Ou | bcftools +fill-tags  -O z -o SNP_INFO_GENO_BI_NOCALL03_FILTERED_RECODED_CLEANED.vcf.gz -- -t AN,AC,AF
+tabix -p vcf SNP_INFO_GENO_BI_NOCALL03_FILTERED_RECODED_CLEANED.vcf.gz
 
+# -- AF filter
+bcftools view \
+  -i 'AF>=0.03 && AF<=0.97' \
+  -O z \
+  -o SNP_INFO_GENO_BI_NOCALL03_FILTERED_RECODED_CLEANED_AF003.vcf.gz \
+  SNP_INFO_GENO_BI_NOCALL03_FILTERED_RECODED_CLEANED.vcf.gz
 
+tabix -p vcf SNP_INFO_GENO_BI_NOCALL03_FILTERED_RECODED_CLEANED_AF003.vcf.gz
 
-# ---------------------------------------------------
-#  filtering SNP step 5 : Sample-level Filtering
-# ---------------------------------------------------
+# ----------- Filter_Step5.sh (END) ---------
 
-# -- list the amount of missing data per individual - find indivdiuals with no reads mapped
-vcftools --vcf Cassiope_noMERhb.recode.vcf --missing-indv --out Cassiope_noMER
-
-
-# -- filter out the individuals with greater than 10% missing SNPs 
-mawk '$5 > 0.1' Cassiope_noMER.imiss | cut -f1 > lowDP.indv
-vcftools --vcf Cassiope_noMERhb.recode.vcf --remove lowDP.indv --recode --recode-INFO-all --out Cassiope_noMER_r10i
-#After filtering, kept 39 out of 40 Individuals
-#After filtering, kept 10857 out of a possible 10857 Sites
+# Check number filtered SNPs left
+bcftools view -H SNP_INFO_GENO_BI_NOCALL03_FILTERED_RECODED_CLEANED_AF003.vcf.gz | wc -l
+# XXX SNPs 
 
 
+
+# --------------------
+# Step 10: SAVE A COPY
+# -------------------
+# Save in three different locations: hard drive, cloud, etc.
 
 # END
 
